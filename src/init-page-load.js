478f0d4b64cb851fdbb2313a129e7c724dbada91
MissLiner <miss.liner@yahoo.com>
2021-09-24 12:35:32 -0400

    Add title and mainImg to ini-page-laod module

---
+++ src/init-page-load.js
@@ -0,0 +1,9 @@
+let contentDiv = document.getElementById('content')
+
+let title = document.createElement('h1');
+title.textContent = 'Novita';
+contentDiv.appendChild(title);
+
+let mainImg = document.createElement('img');
+mainImg.src = 'novita_outdoors.jpeg';
+contentDiv.appendChild(mainImg);
