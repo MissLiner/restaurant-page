12b453af34174b987351839f941e35a81495a6fe
MissLiner <miss.liner@yahoo.com>
2021-09-27 13:47:05 -0400

    Fix bug from csv-parser, add data as array instead

---
+++ src_contact-tab_js.main.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkrestaurant_page"] = self["webpackChunkrestaurant_page"] || []).push([["src_contact-tab_js"],{
+
+/***/ "./src/contact-tab.js":
+/*!****************************!*\
+  !*** ./src/contact-tab.js ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _elements_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./elements.js */ \"./src/elements.js\");\n\n\n_elements_js__WEBPACK_IMPORTED_MODULE_0__.subTitle.textContent = 'Wild treats for all ages!';\n//contentDiv.appendChild(subTitle);\n\n_elements_js__WEBPACK_IMPORTED_MODULE_0__.intro.textContent = 'Bring your camera and your courage, and you never know what sweet surprises you might discover at Annabelle\\'s. Cheetah cupcakes, giraffe lollipops, hippo sundaes - it\\'s just like a trip to the savannah, only sweeter!';\n//contentDiv.appendChild(intro);\n\n//# sourceURL=webpack://restaurant-page/./src/contact-tab.js?");
+
+/***/ })
+
+}]);
