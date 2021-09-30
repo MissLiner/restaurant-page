71ed72eef63a69e443146329c5e2234db7292bec
MissLiner <miss.liner@yahoo.com>
2021-09-30 15:57:08 -0400

    Optimize for landscape mobile

--- main.js
+++ main.js
@@ -16,7 +16,7 @@
   \*************************************************************/
 /***/ ((module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/noSourceMaps.js */ \"./node_modules/css-loader/dist/runtime/noSourceMaps.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);\n// Imports\n\n\nvar ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));\n// Module\n___CSS_LOADER_EXPORT___.push([module.id, \"* {\\n    font-family:sans-serif;\\n    text-align:center;\\n}\\n\\nbody, html {\\n    background-color: #C7E1FF;\\n    height: 100%;\\n    margin: 0;\\n  }\\n\\nh3 {\\n    margin-bottom: 10px;\\n}\\n\\nspan {\\n    background-color: #C7E1FF;\\n    white-space: pre;\\n}\\n\\n.bold {\\n    font-weight:bold;\\n}\\n\\n.menu-name {\\n    text-align: left;\\n}\\n\\n.menu-name:after {\\n    left: 50px;\\n    width: 0;\\n    white-space: nowrap pre;\\n    content:\\n    \\\" . . . . . . . . . . . . . . . . . . . . . . . .\\\"\\n}\\n\\n.menu-price {\\n    text-align: right;\\n}\\n\\n.tab-btn {\\n    background-color:#4CACFF;\\n    color: black;\\n    border: none;\\n    outline: none;\\n    cursor: pointer;\\n    padding: 14px 16px;\\n    font-size: 18px;\\n    width: 33.3%;\\n    margin-top: -5px;\\n}\\n\\n.tab-btn:hover {\\n    background-color:azure;\\n}\\n\\n.selected-tab {\\n    background-color: #C7E1FF;\\n    font-size: larger;\\n    font-weight:bold;\\n}\\n\\n#footer {\\n    position:absolute;\\n    bottom: 0px;\\n    left: 0px;\\n}\\n\\n#intro {\\n    margin:auto;\\n    width: 500px;\\n}\\n\\n.menu {\\n    row-gap: 10px;\\n    display:grid;\\n    grid-template-columns: 50% 50%;\\n    grid-template-rows: auto;\\n    margin: auto;\\n    margin-bottom: 30px;\\n    overflow: hidden;\\n    text-decoration:solid;\\n    white-space: nowrap;\\n    width: 350px;\\n}\\n\\n#contact-info {\\n    display:grid;\\n    grid-template-columns: 1fr 1fr;\\n    grid-template-rows: auto;\\n    margin:auto;\\n    row-gap: 20px;\\n    width: 300px;\\n}\\n\\n@media only screen \\n  and (min-device-width: 375px) \\n  and (max-device-width: 812px) \\n  /* and (-webkit-min-device-pixel-ratio: 3) */\\n  {  \\n*, .tab-btn {\\n    font-size: 42px;\\n}\\n.menu, #intro, #contact-info {\\n    width: 620px;\\n}\\n#subtitle {\\n    margin-top: 150px;\\n}\\n} \", \"\"]);\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);\n\n\n//# sourceURL=webpack://restaurant-page/./src/style.css?./node_modules/css-loader/dist/cjs.js");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/noSourceMaps.js */ \"./node_modules/css-loader/dist/runtime/noSourceMaps.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/getUrl.js */ \"./node_modules/css-loader/dist/runtime/getUrl.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2__);\n// Imports\n\n\n\nvar ___CSS_LOADER_URL_IMPORT_0___ = new URL(/* asset import */ __webpack_require__(/*! ./Barkingcatdemo-lR9w.otf */ \"./src/Barkingcatdemo-lR9w.otf\"), __webpack_require__.b);\nvar ___CSS_LOADER_URL_IMPORT_1___ = new URL(/* asset import */ __webpack_require__(/*! ./Fresca-Regular.ttf */ \"./src/Fresca-Regular.ttf\"), __webpack_require__.b);\nvar ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));\nvar ___CSS_LOADER_URL_REPLACEMENT_0___ = _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default()(___CSS_LOADER_URL_IMPORT_0___);\nvar ___CSS_LOADER_URL_REPLACEMENT_1___ = _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default()(___CSS_LOADER_URL_IMPORT_1___);\n// Module\n___CSS_LOADER_EXPORT___.push([module.id, \"@font-face {\\n    font-family: 'BarkingCat';\\n    src: url(\" + ___CSS_LOADER_URL_REPLACEMENT_0___ + \");\\n}\\n\\n@font-face {\\n    font-family: 'FrescaReg';\\n    src: url(\" + ___CSS_LOADER_URL_REPLACEMENT_1___ + \");\\n}\\n\\n* {\\n    font-family:sans-serif;\\n    text-align:center;\\n}\\n\\nbody, html {\\n    background-color: #C7E1FF;\\n    height: 100%;\\n    margin: 0;\\n  }\\n\\nh3 {\\n    margin-bottom: 10px;\\n}\\n\\nspan {\\n    background-color: #C7E1FF;\\n    white-space: pre;\\n}\\n\\n.bold {\\n    font-weight:bold;\\n}\\n\\n.menu {\\n    row-gap: 10px;\\n    display:grid;\\n    grid-template-columns: 50% 50%;\\n    grid-template-rows: auto;\\n    margin: auto;\\n    margin-bottom: 30px;\\n    overflow: hidden;\\n    text-decoration:solid;\\n    white-space: nowrap;\\n    width: 350px;\\n}\\n\\n.menu-name {\\n    font-family: 'FrescaReg'; \\n    text-align: left;\\n}\\n\\n.menu-name:after {\\n    left: 50px;\\n    width: 0;\\n    white-space: nowrap pre;\\n    content:\\n    \\\" . . . . . . . . . . . . . . . . . . . . . . . .\\\"\\n    \\\" . . . . . . . . . . . . . . . . . . . . . . . .\\\"\\n}\\n\\n.menu-price {\\n    font-family: 'FrescaReg'; \\n    text-align: right;\\n}\\n\\n.tab-btn {\\n    background-color:#4CACFF;\\n    color: black;\\n    border: none;\\n    outline: none;\\n    cursor: pointer;\\n    padding: 14px 16px;\\n    font-size: 18px;\\n    width: 33.3%;\\n    margin-top: -5px;\\n}\\n\\n.tab-btn:hover {\\n    background-color:azure;\\n}\\n\\n.selected-tab {\\n    background-color: #C7E1FF;\\n    font-size: larger;\\n    font-weight:bold;\\n}\\n\\n#footer {\\n    position:absolute;\\n    bottom: 0px;\\n    left: 0px;\\n}\\n\\n#intro {\\n    margin:auto;\\n    width: 500px;\\n}\\n\\n#subtitle {\\n    font-family: 'FrescaReg';\\n    font-size: 40px;\\n}\\n\\n#contact-info {\\n    display:grid;\\n    grid-template-columns: 1fr 1fr;\\n    grid-template-rows: auto;\\n    margin:auto;\\n    row-gap: 20px;\\n    width: 300px;\\n}\\n\\n@media only screen \\n  and (min-device-width: 375px) \\n  and (max-device-width: 812px) \\n  and (orientation: portrait)\\n    {  \\n        *, .tab-btn {\\n            font-size: 42px;\\n        }\\n        .menu, #intro, #contact-info {\\n            width: 700px;\\n        }\\n        #subtitle {\\n            font-size:60px;\\n            margin-top: 75px;\\n        }\\n    } \\n@media only screen \\n    and (min-device-width: 375px) \\n    and (max-device-width: 812px) \\n    and (orientation: landscape) {  \\n        .tab-btn {\\n            font-size: 28px;\\n        }\\n        h3 {\\n            font-size: 24px;\\n        }\\n        #intro, #contact-info {\\n            font-size: 26px;\\n            width: 500px;\\n        }\\n        .menu {\\n            font-size: 24px;\\n            margin-bottom: 5px;\\n            row-gap: 0px;\\n            width: 400px;\\n        }\\n        .menu-name {\\n            margin-bottom: 5px;\\n            margin-top: 5px;\\n        }\\n        h3 {\\n            margin: 0px;\\n        }\\n        #subtitle {\\n            font-size:48px;\\n            margin: 0px;\\n            margin-bottom: 20px;\\n            margin-top: 20px;\\n        }\\n    } \\n\", \"\"]);\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);\n\n\n//# sourceURL=webpack://restaurant-page/./src/style.css?./node_modules/css-loader/dist/cjs.js");
 
 /***/ }),
 
@@ -27,6 +27,16 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /***/ ((module) => {
 
 eval("\n\n/*\n  MIT License http://www.opensource.org/licenses/mit-license.php\n  Author Tobias Koppers @sokra\n*/\nmodule.exports = function (cssWithMappingToString) {\n  var list = []; // return the list of modules as css string\n\n  list.toString = function toString() {\n    return this.map(function (item) {\n      var content = \"\";\n      var needLayer = typeof item[5] !== \"undefined\";\n\n      if (item[4]) {\n        content += \"@supports (\".concat(item[4], \") {\");\n      }\n\n      if (item[2]) {\n        content += \"@media \".concat(item[2], \" {\");\n      }\n\n      if (needLayer) {\n        content += \"@layer\".concat(item[5].length > 0 ? \" \".concat(item[5]) : \"\", \" {\");\n      }\n\n      content += cssWithMappingToString(item);\n\n      if (needLayer) {\n        content += \"}\";\n      }\n\n      if (item[2]) {\n        content += \"}\";\n      }\n\n      if (item[4]) {\n        content += \"}\";\n      }\n\n      return content;\n    }).join(\"\");\n  }; // import a list of modules into the list\n\n\n  list.i = function i(modules, media, dedupe, supports, layer) {\n    if (typeof modules === \"string\") {\n      modules = [[null, modules, undefined]];\n    }\n\n    var alreadyImportedModules = {};\n\n    if (dedupe) {\n      for (var _i = 0; _i < this.length; _i++) {\n        var id = this[_i][0];\n\n        if (id != null) {\n          alreadyImportedModules[id] = true;\n        }\n      }\n    }\n\n    for (var _i2 = 0; _i2 < modules.length; _i2++) {\n      var item = [].concat(modules[_i2]);\n\n      if (dedupe && alreadyImportedModules[item[0]]) {\n        continue;\n      }\n\n      if (typeof layer !== \"undefined\") {\n        if (typeof item[5] === \"undefined\") {\n          item[5] = layer;\n        } else {\n          item[1] = \"@layer\".concat(item[5].length > 0 ? \" \".concat(item[5]) : \"\", \" {\").concat(item[1], \"}\");\n          item[5] = layer;\n        }\n      }\n\n      if (media) {\n        if (!item[2]) {\n          item[2] = media;\n        } else {\n          item[1] = \"@media \".concat(item[2], \" {\").concat(item[1], \"}\");\n          item[2] = media;\n        }\n      }\n\n      if (supports) {\n        if (!item[4]) {\n          item[4] = \"\".concat(supports);\n        } else {\n          item[1] = \"@supports (\".concat(item[4], \") {\").concat(item[1], \"}\");\n          item[4] = supports;\n        }\n      }\n\n      list.push(item);\n    }\n  };\n\n  return list;\n};\n\n//# sourceURL=webpack://restaurant-page/./node_modules/css-loader/dist/runtime/api.js?");
+
+/***/ }),
+
+/***/ "./node_modules/css-loader/dist/runtime/getUrl.js":
+/*!********************************************************!*\
+  !*** ./node_modules/css-loader/dist/runtime/getUrl.js ***!
+  \********************************************************/
+/***/ ((module) => {
+
+eval("\n\nmodule.exports = function (url, options) {\n  if (!options) {\n    options = {};\n  }\n\n  if (!url) {\n    return url;\n  }\n\n  url = String(url.__esModule ? url.default : url); // If url is already wrapped in quotes, remove them\n\n  if (/^['\"].*['\"]$/.test(url)) {\n    url = url.slice(1, -1);\n  }\n\n  if (options.hash) {\n    url += options.hash;\n  } // Should url be wrapped?\n  // See https://drafts.csswg.org/css-values-3/#urls\n\n\n  if (/[\"'() \\t\\n]|(%20)/.test(url) || options.needQuotes) {\n    return \"\\\"\".concat(url.replace(/\"/g, '\\\\\"').replace(/\\n/g, \"\\\\n\"), \"\\\"\");\n  }\n\n  return url;\n};\n\n//# sourceURL=webpack://restaurant-page/./node_modules/css-loader/dist/runtime/getUrl.js?");
 
 /***/ }),
 
@@ -140,6 +150,26 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 
 /***/ }),
 
+/***/ "./src/Barkingcatdemo-lR9w.otf":
+/*!*************************************!*\
+  !*** ./src/Barkingcatdemo-lR9w.otf ***!
+  \*************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"5b9d500fa6d9e624ebbd.otf\";\n\n//# sourceURL=webpack://restaurant-page/./src/Barkingcatdemo-lR9w.otf?");
+
+/***/ }),
+
+/***/ "./src/Fresca-Regular.ttf":
+/*!********************************!*\
+  !*** ./src/Fresca-Regular.ttf ***!
+  \********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"fb8d594963237dfe84f9.ttf\";\n\n//# sourceURL=webpack://restaurant-page/./src/Fresca-Regular.ttf?");
+
+/***/ }),
+
 /***/ "./src/annabelle_logo.svg":
 /*!********************************!*\
   !*** ./src/annabelle_logo.svg ***!
@@ -205,6 +235,9 @@ eval("module.exports = __webpack_require__.p + \"9a5c57e4b8ff16acb32c.svg\";\n\n
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
 /******/ 	/* webpack/runtime/compat get default export */
@@ -277,6 +310,32 @@ eval("module.exports = __webpack_require__.p + \"9a5c57e4b8ff16acb32c.svg\";\n\n
 /******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
 /******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
 /******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	(() => {
+/******/ 		__webpack_require__.b = document.baseURI || self.location.href;
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			"main": 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		// no on chunks loaded
+/******/ 		
+/******/ 		// no jsonp function
 /******/ 	})();
 /******/ 	
 /************************************************************************/
