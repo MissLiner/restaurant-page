c2a115f7a82cc538020af1c92c8008fefe2ef7be
MissLiner <miss.liner@yahoo.com>
2021-10-05 09:31:42 -0400

    adjustImages only with protrait orientation

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ contentDiv.appendChild(contactInfo);
 contentDiv.appendChild(footer);
 
 
-const smallerWindow = window.matchMedia("(max-width: 1000px) and (orientation: portrait");
+const smallerWindow = window.matchMedia("(orientation: portrait");
 
 function adjustImages(dimension) {
     if (dimension.matches) {
